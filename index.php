9be1a6cdaa45d71e3f2fa9ada5ea71a0ca2f23e5
VxneC0de <vanerubio2020@gmail.com>
2024-06-28 16:57:22 -0400

    feat: create 3 variables in php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
   $conex = mysqli_connect("localhost", "root", "", "idsearch_vxnec0de") or die("no se pudo contectar a la DB");
 
+  $sql = "insert into identification values('', 29664547, 'Vanessa', 'Rubio', 0)";
+
+  if(mysqli_query($conex, $sql)){
+    echo  "Se registro con exito";
+  }else{
+    echo "problemas al registrarse, intente nuevamente mas tarde"
+  };
+
+  
+
   /*
+  $ = para crear variables
+
+  $conex es la conexion a la db
+
   mysqli_connect() tiene 4 parametros:
   -el primero es en donde va buscar la base, es en el servidor(localhost),
   -el segundo es el usuario de la base de datos, en este caso la raiz,
@@ -9,5 +23,11 @@
   el cuarto es el nombre de la base de datos
 
   or die() mata todo, manda un mensaje
+
+  mysqli_query es una funcion que se encarga de enviar un query o una colsuta a la base de datos y recibe dos parametros:
+  -parametro 1, la conexion, que en este caso es $conex
+  -parametro 2, insertar dato, lo que quiero hacer con la base de datos, en este caso lo de sql
+
+  debo preguntar a la funcion si trabajo, un booleano, si inserto el dato o no, para eso es el "if, else", para comprobar la condicion si esta trabajando o no.
   */
 ?>
